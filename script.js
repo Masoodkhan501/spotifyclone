78624abbc87ff2a76439e975b347f739fb98d947
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2025-06-19 09:58:23 +0530

    songs are playable but need some changes

--- script.js
+++ script.js
@@ -13,16 +13,23 @@ async function getSongs() {
     }
     return songs
 }
+
+const playMusic=(song_name, song_by)=>
+{
+    let audio = new Audio("/songs/"+song_name);
+    audio.play();
+}
+
 async function main() {
     let songs =  await getSongs();
-    console.log(songs)
+    // console.log(songs)
 
     let songul = document.querySelector(".songlist").getElementsByTagName("ul")[0];
     for (const song of songs) {
         songul.innerHTML += `<li class="flex justify-between px-1 mb-2 cursor-pointer">
         <div class="flex">
             <img src="music.svg" alt="music logo" class="invert-100 pr-3"/>
-            <div>               
+            <div class="info">               
                 <p calss="text-sm">${song.replaceAll("%20"," ").split("-")[0]}</p>
                 <p class="text-xs">${song.replaceAll("%20"," ").split("-")[1].split(".")[0]}</p>
             </div>
@@ -33,16 +40,14 @@ async function main() {
         </div>
         </li>`; 
     }
-
-    // //play the audio
-    // var audio = new Audio(songs[0]);
-    // // audio.play();
-
-    // audio.addEventListener("",()=>
-    // {
-    //     let duration = audio.duration;
-    //     console.log(duration);
-    // })
+    let currentsong;
+    //attach a event listener
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(element => {
+        element.addEventListener("click", e =>{
+            console.log(e.currentTarget.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.currentTarget.querySelector(".info").firstElementChild.innerHTML.trim() +" - "+e.currentTarget.querySelector(".info").lastElementChild.innerHTML.trim()+".mp3");
+        })
+    });
 }
 
 main();
